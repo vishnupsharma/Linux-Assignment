a8d9421fbad8c84111d1841d58e9e479d792e952
Vishnu Prasad Sharma <vishnu@inito.com>
2023-07-28 14:00:38 +0530

    "Adding the modified file for second question"

--- Questiontwo.sh
+++ Questiontwo.sh
@@ -1,20 +1,17 @@
 #! /bin/bash
 # This will run the process
-process_name= "$1"
+process_name=$1
 $process_name
 
 exit_status=$?
 while [ $exit_status != 0 ]
 do
 
-	echo "this is exit status: $exit_status"
-	if [ $exit_status -eq 0 ]; then
-		echo "No crash, everything went good"
-	else
-		echo "it crashed, let's restart"
-		$process_name
-		exit_status=$?
-	fi
+	#echo "this is exit status: $exit_status"
+
+	$process_name
+	exit_status=$?
+	#if[ $? -eq 0]; then
 done
 
 echo "The execution is over"
